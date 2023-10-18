956dc54f397cf2ae50f0fbdc137197215b1de170
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-10-18 22:15:58 +0300

    Implement memory allocation handling and instruction processing in interpret function
    -The `interpret` function has been extended to include a memory allocation handling mechanism.
    
    -Processes instructions from the input file and handles unknown instructions gracefully.

--- interpreter.c
+++ interpreter.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+#include "memory-allocation.h"
 
 
 /**
@@ -10,14 +11,31 @@
  */
 void interpret(FILE *fstream)
 {
-	char *line = NULL;
+	char *token, *line = NULL, **line_ptr = &line;
 	size_t len = 0, line_number = 0;
 	ssize_t nread;
+	long linep_alloc_id;
+	void (*op_func)(INSTRUCTION_ARGS *);
+
+	linep_alloc_id = push_allocated_memory(
+		create_allocated_memory(STRING_POINTER, line_ptr)
+	);
 
 	while ((nread = getline(&line, &len, fstream)) != -1)
 	{
 		line_number++;
-		printf("%s", line);
+		token = strtok(line, " $\n");
+		op_func = get_op_function(token);
+
+		if (op_func == NULL)
+		{
+			fprintf(stderr, "L%lu: unknown instruction %s\n",
+				line_number, token);
+			clean_allocated_memory();
+			exit(EXIT_FAILURE);
+		}
+
 	}
+	deallocate_memory(linep_alloc_id);
 	free(line);
 }
